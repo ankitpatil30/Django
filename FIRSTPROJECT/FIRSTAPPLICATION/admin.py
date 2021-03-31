0732dd4224e96ef751f30a2e61cd417e58781161
Ankit Patil <ankitmpatil@gmail.com>
2021-03-31 01:57:10 -0500

    3 Tables Created

--- FIRSTPROJECT/FIRSTAPPLICATION/admin.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from FIRSTAPPLICATION.models import Signup
+from FIRSTAPPLICATION.models import Signup, Technology, Student
 # Register your models here.
 
 admin.site.register(Signup)
+admin.site.register(Technology)
+admin.site.register(Student)
--- FIRSTPROJECT/FIRSTAPPLICATION/models.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/models.py
@@ -7,3 +7,17 @@ class Signup(models.Model):
     email=models.EmailField(max_length=40)
     password=models.CharField(max_length=30)
     release_date = models.DateField()
+
+class Technology(models.Model):
+    session=models.CharField(max_length=40,unique=True)
+    
+    def __str__(self):
+        return self.session
+
+
+class Student(models.Model):
+    name=models.CharField(max_length=30, unique=False)
+    session=models.ForeignKey("Technology", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
